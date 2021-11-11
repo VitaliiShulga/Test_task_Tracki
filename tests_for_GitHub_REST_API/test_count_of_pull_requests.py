c243ab65a9c757a6ce5331b9fc4ab5087c85ac42
Jon Jon <jon@exenple.com>
2021-11-11 17:17:44 +0200

    added correctly pulls tests

--- tests_for_GitHub_REST_API/test_count_of_pull_requests.py
+++ tests_for_GitHub_REST_API/test_count_of_pull_requests.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
 import requests
 
-count_open = 0
-count_close = 0
-username = 'Fantomas42'
-repo = 'django-blog-zinnia'
+def count_open_pulls():
+    username = 'Fantomas42'
+    repo = 'django-blog-zinnia'
+    count_open = 0
+    url_open = f'https://api.github.com/repos/{username}/{repo}/pulls?state=open'
+    open_data = requests.get(url_open).json()
 
-url_open = f'https://api.github.com/repos/{username}/{repo}/pulls?state=open'
-open_data = requests.get(url_open).json()
+    for request in open_data:
+        for key, value in request.items():
+            if key == 'state' and value == 'open':
+                count_open += 1
+    return count_open
 
-for request in open_data:
-    for key, value in request.items():
-         if key == 'state' and value == 'open':
-            count_open += 1
-
-for page_num in range(2):
-    url = f'https://api.github.com/repos/{username}/{repo}/pulls?state=closed&per_page=100&page={page_num}'
+def count_closed_pulls():
+    username = 'Fantomas42'
+    repo = 'django-blog-zinnia'
+    count_close = 0
+    url = f'https://api.github.com/repos/{username}/{repo}/pulls?state=closed&per_page=100&page=1'
     data = requests.get(url).json()
 
     for request in data:
@@ -23,12 +26,25 @@ for page_num in range(2):
             if key == 'state' and value == 'closed':
                 count_close += 1
 
+    url = f'https://api.github.com/repos/{username}/{repo}/pulls?state=closed&per_page=100&page=2'
+    data = requests.get(url).json()
+
+    for request in data:
+        for key, value in request.items():
+            if key == 'state' and value == 'closed':
+                count_close += 1
+
+    return count_close
+
+
+
 class TestsCountsPullRequests:
     def test_count_of_open_pull_requests(self):
         self.driver = webdriver.Chrome()
         self.driver.get('https://github.com/Fantomas42/django-blog-zinnia/pulls')
         open = self.driver.find_element_by_xpath('//a [@class="btn-link selected"]')
-        assert int(open.text.split()[0]) == count_open
+        open = open.text.split()[0]
+        assert int(open) == count_open_pulls(), f'count open pull in web == {int(open)} this != {count_open_pulls()}'
         self.driver.close()
 
     def test_count_of_closed_pull_requests(self):
@@ -36,6 +52,6 @@ class TestsCountsPullRequests:
         self.driver.get('https://github.com/Fantomas42/django-blog-zinnia/pulls')
         closed = self.driver.find_element_by_xpath('//a [@class="btn-link "]')
         closed = closed.text.split()[0]
-        assert int(closed) == count_close
+        assert int(closed) == count_closed_pulls(), f'count closed pull in web == {int(closed)} this != {count_closed_pulls()}'
         self.driver.close()
 
